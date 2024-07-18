1f1cc0c9e78519c7138620eac7272160e7691f40
vishalmet <vishalmet003@gmail.com>
2024-07-18 12:40:22 +0530

    bg

--- src/components/GradientBg.jsx
+++ src/components/GradientBg.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const GradientBackground = ({ children }) => {
   return (
-    <div className="relative w-full min-h-screen  bg-white overflow-hidden">
+    <div className="relative w-full min-h-screen  bg-gradient-to-tr from-black via-gray-800 to-gray-600 overflow-hidden">
       <div className="absolute top-0 left-0 w-[150px] h-[150px] md:w-[400px] md:h-[400px] bg-gradient-to-r from-purple-800 via-purple-500 to-pink-500 rounded-full blur-3xl opacity-70 animate-gradient1"></div>
       <div className="absolute top-1/4 left-0 w-[150px] h-[150px] md:w-[400px] md:h-[400px] bg-gradient-to-r from-blue-500 to-green-500 rounded-full blur-3xl opacity-70 animate-gradient1"></div>
       <div className="absolute top-1/4 left-1/2 w-[150px] h-[150px] md:w-[500px] md:h-[500px] bg-gradient-to-r from-blue-500 to-green-500 rounded-full blur-3xl opacity-70 animate-gradient2"></div>
