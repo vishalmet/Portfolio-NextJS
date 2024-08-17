a60c370d65b48b49a4fa56ffc52ea793d25c30ce
vishalmet <vishalmet003@gmail.com>
2024-08-17 22:12:48 +0530

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
-import Home from "./Home/Home";
+import Home from "./Home/Home.jsx";
 // import Loader from "@/Components/Loader";
 import "@/app/globals.css";
 // import { Analytics } from "@vercel/analytics/react";
