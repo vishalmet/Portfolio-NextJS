a9556b348935cbd17c576b5663a5e35273a43959
vishalmet <vishalmet003@gmail.com>
2024-08-19 09:55:38 +0530

    bug fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,4 @@
-"use client";
-import { useState, useEffect } from "react";
-import Home from "./Home/Home.jsx";
-// import Loader from "@/Components/Loader";
-import "@/app/globals.css";
-// import { Analytics } from "@vercel/analytics/react";
+import Head from 'next/head';
 
 export default function Page() {
   const [loading, setLoading] = useState(true);
@@ -25,17 +20,15 @@ export default function Page() {
 
   return (
     <>
-      <head>
+      <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:opsz,wght@12..96,200..800&display=swap"
           rel="stylesheet"
         />
-      </head>
+      </Head>
       <main className="">
-        {/* <Analytics /> */}
-        {/* {loading && <Loader />} */}
         <div className={loading ? "blur-sm" : ""}>
           <Home />
         </div>
