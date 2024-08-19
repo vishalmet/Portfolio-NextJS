856e87cb98c551514dd4d9306e28d5401fa6c586
vishalmet <vishalmet003@gmail.com>
2024-08-19 11:44:02 +0530

    bug fix

--- src/app/page.js
+++ src/app/page.js
@@ -14,6 +14,7 @@ export default function Page() {
         setLoading(false);
       }, 0);
     };
+    document.title = "Home Page";
 
     if (document.readyState === "complete") {
       handleComplete();
