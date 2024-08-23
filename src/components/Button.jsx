b306ee67f6d2901564dfaeaf5bae6137d5661a66
vishalmet <vishalmet003@gmail.com>
2024-08-23 12:22:25 +0530

    noopener

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from "react";
 import { motion } from "framer-motion";
 import { useRouter } from "next/navigation";
@@ -6,30 +6,28 @@ import { useRouter } from "next/navigation";
 const ButtonTemp = ({ title, to }) => {
   const router = useRouter();
 
-  const handleClick = () => {
+  const handleClick = (e) => {
     if (typeof window !== "undefined") {
       if (to.startsWith("http") || to.startsWith("https")) {
-        window.location.href = to;
+        e.preventDefault(); // Prevent default anchor behavior
+        window.open(to, "_blank", "noopener,noreferrer"); // Open in a new tab
       } else {
-        router.push(to);
+        router.push(to); // Handle internal routing
       }
     }
   };
 
   return (
-    <div>
-      <div className="flex justify-center pt-3 md:pt-6 brandy-font">
-        <motion.button
-          className="text-white bg-gradient-to-t from-pink-500 via-red-500 to-yellow-500 hover:bg-gradient-to-b hover:shadow-orange-500 hover:scale-95 hover:shadow-xl p-3 rounded-xl text-xs md:text-base border-b-2 border-white custom-border-radius"
-          whileTap={{ scale: 0.9 }}
-          onClick={handleClick}
-        >
-          {title}
-        </motion.button>
-      </div>
+    <div className="flex justify-center pt-3 md:pt-6 brandy-font">
+      <motion.button
+        className="text-white bg-gradient-to-t from-pink-500 via-red-500 to-yellow-500 hover:bg-gradient-to-b hover:shadow-orange-500 hover:scale-95 hover:shadow-xl p-3 rounded-xl text-xs md:text-base border-b-2 border-white custom-border-radius"
+        whileTap={{ scale: 0.9 }}
+        onClick={handleClick}
+      >
+        {title}
+      </motion.button>
     </div>
   );
 };
 
 export default ButtonTemp;
-
