406cbaa88c66453949c0ae6dcdea159a2d8ad907
Vishal Aakash <111877553+vishalmet@users.noreply.github.com>
2024-08-26 21:12:45 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className=' bg-gradient-to-t from-pink-500 via-red-500 to-yellow-500 p-3 mt-10'>
+    <div className=' bg-gradient-to-t from-pink-500 via-red-500 to-yellow-500 p-3 mt-3 lg:mt-10'>
         <p className=' text-center'>Designed & Developed by Vishal </p>
     </div>
   )
