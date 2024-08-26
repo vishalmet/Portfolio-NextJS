40a17912f63a25d527d349cfe8b177b0a50a12f4
vishalmet <vishalmet003@gmail.com>
2024-08-26 21:08:16 +0530

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className=' bg-gradient-to-t from-pink-500 via-red-500 to-yellow-500 p-3 mt-10'>
+        <p className=' text-center'>Designed & Developed by Vishal </p>
+    </div>
+  )
+}
+
+export default Footer
