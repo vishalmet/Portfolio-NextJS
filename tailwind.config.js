10b5342b01e7b91d59bd875a329baf46a2ea3154
vishalmet <vishalmet003@gmail.com>
2024-09-23 15:27:31 +0530

    grid bg sset

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,9 +14,8 @@ module.exports = {
         sans: ['Bricolage Grotesque', 'sans-serif'],
       },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       keyframes: {
         gradient1: {
