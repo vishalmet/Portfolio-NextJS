618f5027f01a673f0764dc6cf068a690ec54ef61
vishalmet <vishalmet003@gmail.com>
2024-09-23 10:50:49 +0530

    ui change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className=' bg-gray-900/50 p-3 mt-10 flex justify-center border-y-2 border-white'>
-        <p className=' text-center bg-gradient-to-t from-pink-500 via-red-400 to-yellow-500 rounded p-3 px-20 w-fit'>Designed & Developed by Vishal </p>
+        <p className=' text-center bg-gradient-to-t from-pink-500 via-red-400 to-yellow-500 rounded p-3 px-0 md:px-20 w-fit text-xs md:text-base'>Designed & Developed by Vishal </p>
     </div>
   )
 }
