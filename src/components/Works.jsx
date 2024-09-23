05823ba3f3b961e6fed45fa2fd60b60d74941baa
vishalmet <vishalmet003@gmail.com>
2024-09-23 15:27:07 +0530

    work template change

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -87,7 +87,7 @@ const Page = () => {
   };
 
   return (
-    <div className="bg-black text-white min-h-screen">
+    <div className="bg-black text-white h-full lg:min-h-screen">
       <div className="relative bg-transparent text-white flex justify-center items-center brandy-font p-4 border-y-2 border-white h-24 lg:h-32">
         <p className="z-10 text-xl md:text-3xl lg:text-6xl bg-gradient-to-t from-pink-500 via-red-400 to-yellow-500 rounded p-3">✦ My Works ✦</p>
         <div className="absolute top-0 left-0 w-full h-full flex items-center">
--- src/components/WorksCard2.jsx
+++ src/components/WorksCard2.jsx
@@ -4,28 +4,36 @@ import ButtonTemp from "./Button";
 
 const CardTemp = ({ imageSrc, title, description, url }) => {
   return (
-    
+
     <div className=" flex justify-center w-full lg:max-w-7xl">
-      <div className="container w-[370px] md:w-[550px] h-[450px] md:h-[550px] flex justify-center items-center relative mx-2 md:mx-6 hover:shadow-orange-500 hover:shadow-2xl transition-transform duration-500 mt-10">
-        <div className="bg bg-transparent w-[360px] md:w-[540px] h-[440px] md:h-[540px] flex justify-center relative">
+      <div className="container w-[370px] md:w-[550px] h-[420px] md:h-[520px] flex justify-center items-center relative mx-2 md:mx-6 hover:shadow-orange-500 hover:shadow-2xl transition-transform duration-500 mt-10">
+        <div className="bg bg-transparent w-[360px] md:w-[540px] h-[410px] md:h-[510px] flex justify-center relative">
           <div className="items-center">
             <div className="">
               <div className="w-[360px] md:w-[540px]">
-              <Image className="w-[360px] md:w-[540px] object-cover transform transition-transform duration-300 hover:scale-105 h-auto" src={imageSrc} alt={title} />
+                <Image className="w-[360px] md:w-[540px] h-[195px] lg:h-[280px] object-cover transform transition-transform duration-300 hover:scale-105" src={imageSrc} alt={title} />
               </div>
-              <div className=" p-3">
-              <p className="text-xl md:text-2xl lg:text-3xl font-semibold brandy-font text-center pt-2">
-                {title}
-              </p>
-              <p className="text-center">{description}</p>
-            </div>
-            <a href={url} target="_blank" rel="noopener noreferrer" className="">
-              <ButtonTemp title="VIEW"></ButtonTemp>
-            </a>
+              <div className="p-1 px-3 ">
+                <p className="text-xl md:text-2xl lg:text-3xl font-semibold brandy-font text-center pt-2">
+                  {title}
+                </p>
+
+                {/* Scrollable and truncated description */}
+                <div className="text-center h-full lg:h-[90px] max-h-[100px] overflow-y-auto scrollbar-thin custom-scrollbar">
+                  <p className="overflow-hidden">
+                    {description}
+                  </p>
+                </div>
+
+              </div>
+
+              <a href={url} target="_blank" rel="noopener noreferrer" className="">
+                <ButtonTemp title="VIEW"></ButtonTemp>
+              </a>
             </div>
           </div>
         </div>
-        <div className="blob w-[370px] md:w-[550px] h-[450px] md:h-[550px] absolute top-0 left-0"></div>
+        <div className="blob w-[370px] md:w-[550px] h-[420px] md:h-[520px] absolute top-0 left-0"></div>
       </div>
     </div>
   );
