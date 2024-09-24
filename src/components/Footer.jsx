8fe38a04e15b5d7fa2b9e8996459ad99a4d63a4e
vishalmet <vishalmet003@gmail.com>
2024-09-24 22:54:45 +0530

    bg change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className=' bg-gray-900/50 p-3 flex justify-center border-y-2 border-white'>
+    <div className=' bg-black p-3 flex justify-center border-y-2 border-white'>
         <p className=' text-center bg-gradient-to-t from-pink-500 via-red-400 to-yellow-500 rounded p-3 px-0 md:px-20 w-fit text-xs md:text-base'>Designed & Developed by Vishal </p>
     </div>
   )
