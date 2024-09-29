06ab7a7ae774d043a81c0f70a540d54cadd7ad06
vishalmet <vishalmet003@gmail.com>
2024-09-29 23:43:07 +0530

    organizing assets

---
+++ src/components/Assets.jsx
@@ -0,0 +1,33 @@
+import CryptCoffee from "@/assets/cryptcoffee.png";
+import DeCric from "@/assets/DeCric.jpeg";
+import FundRaiz from "@/assets/FundRaiz.jpeg";
+import GrFolio from "@/assets/grfolio.jpeg";
+import Grid from "@/assets/grid.webp";
+import IG from "@/assets/IG.png";
+import Me from "@/assets/me.png"
+import OnchainMemes from "@/assets/onchainmemes.png"
+import OneSign from "@/assets/onesign.png"
+import Ticket3 from "@/assets/ticket3.jpeg"
+import Tesla from "@/assets/tesla.png"
+import VALogo from "@/assets/VAlogo.png"
+import VortexEngine from "@/assets/vortexengine.png"
+import ZkSpin from "@/assets/zkspin.png"
+
+const Assets = {
+CryptCoffee,
+DeCric,
+FundRaiz,
+GrFolio,
+Grid,
+IG,
+Me,
+OnchainMemes,
+OneSign,
+Ticket3,
+Tesla,
+VALogo,
+VortexEngine,
+ZkSpin
+};
+
+export default Assets;
