153c0811bff37dc52d7ad355c621caf4eca39eb8
vishalmet <vishalmet003@gmail.com>
2024-12-10 20:56:31 +0530

    build error fix

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,19 +5,22 @@ import { useRouter } from "next/navigation";
 
 const ButtonTemp = ({ title, to }) => {
   const router = useRouter();
-  const [loading, setLoading] = useState(false); // Add loading state
+  const [loading, setLoading] = useState(false);
 
   const handleClick = (e) => {
-    setLoading(true); // Start loading when the button is clicked
+    setLoading(true);
 
     if (typeof window !== "undefined") {
       if (to.startsWith("http") || to.startsWith("https")) {
-        e.preventDefault(); // Prevent default anchor behavior
-        window.open(to, "_blank", "noopener,noreferrer"); // Open in a new tab
-        setLoading(false); // Stop loading after the tab is opened
+        e.preventDefault();
+        window.open(to, "_blank", "noopener,noreferrer");
+        setLoading(false);
       } else {
-        router.push(to); // Handle internal routing
+        router.push(to);
       }
+    } else {
+      // Handle the case when window is not defined (server-side)
+      setLoading(false);
     }
   };
 
@@ -27,9 +30,9 @@ const ButtonTemp = ({ title, to }) => {
         className="text-white items-center bg-gradient-to-t from-pink-500 via-red-500 to-yellow-500 hover:bg-gradient-to-b hover:shadow-orange-500 hover:scale-95 hover:shadow-xl p-3 px-6 rounded-xl text-xs md:text-sm lg:text-base border-b-2 border-white custom-border-radius"
         whileTap={{ scale: 0.9 }}
         onClick={handleClick}
-        disabled={loading} // Disable the button while loading
+        disabled={loading}
       >
-        {loading ? ( // Show spinner when loading is true
+        {loading ? (
           <div className="flex justify-center items-center">
             <div className="w-4 h-4 border-2 border-t-transparent border-white border-solid rounded-full animate-spin"></div>
             <span className="ml-2">Loading...</span>
